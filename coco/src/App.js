de35999108c7636624563528c2acaf3247fd7036
MINSU <ancx1234@naver.com>
2022-11-07 22:49:00 +0900

    <Feat> : dev-path세팅
    <Feat> : dev-path세팅
    
    App.js 초기화, Path 세팅
    
    Path세팅 되어있는 부분 수정하면서 자기가 만드는 부분 만들면 될 듯

--- coco/src/App.js
+++ coco/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+// 여기서 path 변경해서 사용 -- minsu
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from './components/Home';
+import { Goods } from './components/Goods';
+import { About } from './components/About';
+import {Service} from './components/Service';
+import {Personal} from './components/Personal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/goods" element={<Goods/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/personal" element={<Personal/>}></Route>
+          <Route path="/service" element={<Service/>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

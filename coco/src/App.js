819ee309e6843384c85ba7e315cbb84d60a6dfe7
kkm0406 <sdjmc30412@naver.com>
2023-01-05 23:36:37 +0900

    <Feat> :  페이징 ---------------------------------------- 1. 채점상황 Pagination 구현
    ----------------------------------------
    1. 문제 페이지 Pagination 구현

--- coco/src/App.js
+++ coco/src/App.js
@@ -1,3 +1,68 @@
+// // 여기서 path 변경해서 사용 -- minsu
+// import "./App.css";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// import { useAppDispatch, useAppSelector } from "./app/store";
+// import { checkToken } from "./app/authentication";
+
+// import { AuthRouter } from "./components/Auth/AuthRouter";
+// import { Home } from "./components/Home/Home";
+// import { Login } from "./components/Login/Login";
+// import { Problems } from "./components/Problems/Problems";
+// import { PBD } from "./components/ProblemDetail/PBD";
+// import { Result } from "./components/Result/Result";
+// import { Manage } from "./components/Manage/Manage";
+// import { Board } from "./components/Board/Board";
+// import { BoardDetail } from "./components/Board/BoardDetail/BoardDetail";
+// import { WriteGeul } from "./components/Board/WriteGeul/WriteGuel";
+// import { StatusList } from "./components/StatusList/StatusList";
+
+// function App() {
+//   const dispatch = useAppDispatch();
+//   const userInfo = useAppSelector((state) => state.loginState);
+//   if (!checkToken(userInfo.access_token)) {
+//     dispatch({
+//       type: "loginSlice/logout",
+//     });
+//   }
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/problems" element={<Problems />} />
+//           <Route path="/problems/:id" element={<PBD />} />
+//           <Route
+//             path="/result/:id"
+//             element={
+//               <AuthRouter role={0}>
+//                 <Result />
+//               </AuthRouter>
+//             }
+//           />
+//           <Route path="/board" element={<Board />} />
+//           <Route path="/board/:id" element={<BoardDetail />} />
+//           <Route path="/manage" element={<Manage />} />
+//           <Route
+//             path="/write"
+//             element={
+//               <AuthRouter role={0}>
+//                 <WriteGeul />
+//               </AuthRouter>
+//             }
+//           />
+//           <Route path="/status" element={<StatusList />} />
+//           <Route path="*" element={<div>없는 페이지입니다.</div>} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 // 여기서 path 변경해서 사용 -- minsu
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -44,14 +109,7 @@ function App() {
           />
           <Route path="/board" element={<Board />} />
           <Route path="/board/:id" element={<BoardDetail />} />
-          <Route
-            path="/manage"
-            element={
-              <AuthRouter role={100}>
-                <Manage />
-              </AuthRouter>
-            }
-          />
+          <Route path="/manage" element={<Manage />} />
           <Route
             path="/write"
             element={
--- coco/src/components/StatusList/StatusList.jsx
+++ coco/src/components/StatusList/StatusList.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Header } from "../Home/Header";
 import { Footer } from "../Home/Footer";
 import "./StatusList.css";
@@ -6,6 +6,7 @@ import { StatusListBox } from "./StatusListBox";
 import Spinner from "react-bootstrap/Spinner";
 import fetchData from "../../api/fetchTask";
 import { useLocation } from "react-router-dom";
+import Pagination from "@mui/material/Pagination";
 
 export const StatusList = () => {
   const location = useLocation();
@@ -41,11 +42,30 @@ export const StatusList = () => {
 
 const Getsubmits = ({ resource }) => {
   const problemList = resource.read();
+  const maxPage = Math.ceil(problemList.length/10);
+  const [page, setPage] = useState(1);
+
+  const handlePage = (event) => {
+    if(event.target.innerHTML === '<path d=\"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z\"></path>'){
+      setPage(page-1)
+    }else if(event.target.innerHTML === '<path d=\"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z\"></path>'){
+      setPage(page+1)
+    }else{
+      setPage(parseInt(event.target.outerText));
+    }
+  }
+    
   return (
     <>
-      {problemList.map((e) => {
-        return <StatusListBox info={e} key={e.user_id} />;
+      {problemList.slice(10*(page-1), 10*(page-1)+10).map((e) => {
+        return <StatusListBox info={e} key={e.sub_id} />;
       })}
+      <div className="pageController">
+        <Pagination count={maxPage} 
+          variant="outlined" shape="rounded" 
+          defaultPage={1} onChange={(e) => handlePage(e)}
+        />
+      </div>
     </>
   );
 };

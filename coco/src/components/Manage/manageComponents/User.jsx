4de2db96f7364a39d07b59300e7e35bd0daeb5dd
kkm0406 <sdjmc30412@naver.com>
2023-06-20 16:38:15 +0900

    <Feat> :  관리자 페이지 ---------------------------------------- 1. 유저 관리 메뉴에서 유저 검색
    ----------------------------------------
    1. 앞으로 할 일

--- coco/src/components/Manage/manageComponents/User.jsx
+++ coco/src/components/Manage/manageComponents/User.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import Spinner from "react-bootstrap/Spinner";
 import fetchData from "../../../api/fetchTask";
 import { GoSearch } from "react-icons/go";
 import { HiUserPlus, HiUserMinus } from "react-icons/hi2";
 import Form from "react-bootstrap/Form";
-import { useState } from "react";
 import axios from "axios";
+import { useAppSelector } from "../../../app/store";
 
 export const User = () => {
+  const userInfo = useAppSelector((state) => state.loginState);
   const [order, setOrder] = useState([]);
+
   const onSearchHandler = (info) => {
     axios
-      .post("http://127.0.0.1:8000/find_task/", {
-        info: info,
-      })
+      .post(
+        "http://127.0.0.1:8000/manage/search_user/",
+        {
+          user_id: info,
+        }
+        // {
+        //   headers: { Authorization: "Bearer " + userInfo.access_token },
+        // }
+      )
       .then((res) => {
         setOrder(res.data);
       });
@@ -27,11 +35,25 @@ export const User = () => {
           <div className="box">
             <p>유저 목록</p>
             <SearchBar search={onSearchHandler} />
-            <UserList />
+            {order.length === 0 ? (
+              <Suspense fallback={<Spinner />}>
+                <UserList
+                  resource={fetchData(`http://127.0.0.1:8000/manage/user_list`)}
+                />
+              </Suspense>
+            ) : (
+              <SearchList order={order} />
+            )}
           </div>
           <div className="box">
             <p>관리자 목록</p>
-            <Managers />
+            <Suspense fallback={<Spinner />}>
+              <Managers
+                resource={fetchData(
+                  `http://127.0.0.1:8000/manage/manager_list`
+                )}
+              />
+            </Suspense>
           </div>
         </div>
       </div>
@@ -59,8 +81,61 @@ const SearchBar = ({ search }) => {
   );
 };
 
-const UserList = () => {
-  const addMananger = (e) => {};
+const SearchList = ({order}) => {
+  const addMananger = (user_id) => {
+    axios
+    .post("http://127.0.0.1:8000/manage/add_manager/", {
+      user_id: user_id
+    })
+    .then((res) => {
+      if(res.data === true){
+        alert(`${user_id}님을 관리자에 추가하였습니다`);
+      }
+    }).catch(() => {
+      alert("관리자 추가에 실패하였습니다.")
+    })
+  };
+  return (
+    <div className="userList-manage">
+      <div className="uL-top">
+        <p>ID</p>
+        <p>Name</p>
+        <p>관리자 추가</p>
+      </div>
+
+      {order.map((e) => {
+        return (
+          <div className="uL-content">
+            <p>{e.id}</p>
+            <p>{e.name}</p>
+            <p
+              style={{ cursor: "pointer", color: "skyblue" }}
+              onClick={() => addMananger(e.id)}
+            >
+              <HiUserPlus size={23} />
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const UserList = ({ resource }) => {
+  const addMananger = (user_id) => {
+    axios
+    .post("http://127.0.0.1:8000/manage/add_manager/", {
+      user_id: user_id
+    })
+    .then((res) => {
+      if(res.data === true){
+        alert(`${user_id}님을 관리자에 추가하였습니다`);
+      }
+    }).catch(() => {
+      alert("관리자 추가에 실패하였습니다.")
+    })
+  };
+  const users = resource.read();
 
   return (
     <div className="userList-manage">
@@ -70,22 +145,40 @@ const UserList = () => {
         <p>관리자 추가</p>
       </div>
 
-      <div className="uL-content">
-        <p>test</p>
-        <p>test</p>
-        <p
-          style={{ cursor: "pointer", color: "skyblue" }}
-          onClick={() => addMananger()}
-        >
-          <HiUserPlus size={23} />
-        </p>
-      </div>
+      {users.map((e) => {
+        return (
+          <div className="uL-content">
+            <p>{e.id}</p>
+            <p>{e.name}</p>
+            <p
+              style={{ cursor: "pointer", color: "skyblue" }}
+              onClick={() => addMananger()}
+            >
+              <HiUserPlus size={23} />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-const Managers = () => {
-  const minusMananger = (e) => {};
+const Managers = ({ resource }) => {
+  const managers = resource.read();
+  const minusMananger = (user_id) => {
+    axios
+    .post("http://127.0.0.1:8000/manage/delete_manager/", {
+      user_id: user_id
+    })
+    .then((res) => {
+      if(res.data === true){
+        alert(`${user_id}님을 관리자에서 삭제하였습니다`);
+      }
+    }).catch(() => {
+      alert("관리자 삭제에 실패하였습니다.")
+    })
+  };
+  
   return (
     <div className="userList-manage" id="mm">
       <div className="uL-top">
@@ -94,16 +187,20 @@ const Managers = () => {
         <p>관리자에서 제거</p>
       </div>
 
-      <div className="uL-content">
-        <p>test</p>
-        <p>test</p>
-        <p
-          style={{ cursor: "pointer", color: "orange" }}
-          onClick={() => minusMananger()}
-        >
-          <HiUserMinus size={23} />
-        </p>
-      </div>
+      {managers.map((e) => {
+        return (
+          <div className="uL-content">
+            <p>{e.id}</p>
+            <p>{e.name}</p>
+            <p
+              style={{ cursor: "pointer", color: "orange" }}
+              onClick={() => minusMananger(e.id)}
+            >
+              <HiUserMinus size={23} />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 };

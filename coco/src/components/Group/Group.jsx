78fa77563b25db2770bfca62c0f67bf1f6a3b247
styughjvbn <3338761@naver.com>
2023-08-25 16:30:51 +0900

    <feat> : 스터디룸 페이지네이션 CO-61 ---------------------------------------- 1. 스터디룸 메인 페이지 2. 스터디룸 개설 페이지 페이지네이션 적용 ----------------------------------------

--- coco/src/components/Group/Group.jsx
+++ coco/src/components/Group/Group.jsx
@@ -19,6 +19,7 @@ import { API } from "api/config";
 export const Group = () => {
   const navigate = useNavigate();
   const userInfo = useAppSelector((state) => state.loginState);
+  const [page, setPage] = useState(1);
   const reload = () => {
     navigate(0);
   };
@@ -75,7 +76,16 @@ export const Group = () => {
                 </div>
 
                 <Suspense fallback={<Spinner />}>
-                  <GetGroups resource={fetchData(API.ROOM)} />
+                  <GetGroups
+                    resource={fetchData(API.ROOM, {
+                      params: {
+                        size: 1,
+                        page: page,
+                      },
+                    })}
+                    setPage={setPage}
+                    page={page}
+                  />
                 </Suspense>
               </div>
             </div>
@@ -138,38 +148,22 @@ const SearchBar = ({ search }) => {
   );
 };
 
-const GetGroups = ({ resource }) => {
+const GetGroups = ({ resource, page, setPage }) => {
   const GroupList = resource.read();
-  const maxPage = Math.ceil(GroupList.length / 6);
-  const [page, setPage] = useState(1);
-  const handlePage = (event) => {
-    if (
-      event.target.innerHTML ===
-      '<path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>'
-    ) {
-      setPage(page - 1);
-    } else if (
-      event.target.innerHTML ===
-      '<path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>'
-    ) {
-      setPage(page + 1);
-    } else {
-      setPage(parseInt(event.target.outerText));
-    }
-  };
 
   return (
     <>
-      {GroupList.slice(5 * (page - 1), 5 * (page - 1) + 5).map((e) => {
+      {GroupList.roomlist.map((e) => {
         return <AllGroupBox info={e} key={e.id} />;
       })}
       <div className="leftBottom" style={{ marginTop: "20px" }}>
         <Pagination
-          count={maxPage}
+          count={Math.ceil(GroupList.total / GroupList.size)}
           variant="outlined"
           shape="rounded"
           defaultPage={1}
-          onChange={(e) => handlePage(e)}
+          page={page}
+          onChange={(e, value) => setPage(value)}
         />
       </div>
     </>
@@ -178,38 +172,12 @@ const GetGroups = ({ resource }) => {
 
 const GetMyGroups = ({ resource }) => {
   const GroupList = resource.read();
-  const maxPage = Math.ceil(GroupList.length / 10);
-  const [page, setPage] = useState(1);
-  const handlePage = (event) => {
-    if (
-      event.target.innerHTML ===
-      '<path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>'
-    ) {
-      setPage(page - 1);
-    } else if (
-      event.target.innerHTML ===
-      '<path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>'
-    ) {
-      setPage(page + 1);
-    } else {
-      setPage(parseInt(event.target.outerText));
-    }
-  };
 
   return (
     <>
-      {GroupList.slice(10 * (page - 1), 10 * (page - 1) + 10).map((e) => {
+      {GroupList.map((e) => {
         return <GroupBox info={e} key={e.id} />;
       })}
-      <div className="leftBottom" style={{ marginTop: "20px" }}>
-        <Pagination
-          count={maxPage}
-          variant="outlined"
-          shape="rounded"
-          defaultPage={1}
-          onChange={(e) => handlePage(e)}
-        />
-      </div>
     </>
   );
 };

083798f5bed5ed93f78dc03c975a220a830622dd
styughjvbn <3338761@naver.com>
2023-09-03 22:14:37 +0900

    <feat> : 스터디룸 QA 목록 조회시 페이지네이션 적용 ---------------------------------------- 1. 스터디룸 QA 목록 조회시 페이지네이션 적용 CO-61 2. 질문 code도 같이 보이도록 수정

--- coco/src/components/Group/QA/QA.jsx
+++ coco/src/components/Group/QA/QA.jsx
@@ -10,54 +10,40 @@ import axios from "axios";
 import { useAppSelector } from "../../../app/store";
 import { API } from "api/config";
 import { useQuery } from "@tanstack/react-query";
+import { BsDashCircle } from "react-icons/bs";
 
 export const QA = () => {
   var path = window.location.pathname;
   path = path.split("/");
+  const [page, setPage] = useState(1);
   const { data } = useQuery(
-    ["qa"],
+    ["qa", path.at(-1), page],
     () => {
-      return axios.get(API.ROOMQUESTION + path.at(-1));
+      return axios.get(API.ROOMQUESTION + path.at(-1), {
+        params: { size: 1, page: page },
+      });
     },
     {
       suspense: true,
     }
   );
-  // const maxPage = Math.ceil(problemList.length / 10);
-  const maxPage = 10;
-  const [page, setPage] = useState(1);
-  const handlePage = (event) => {
-    if (
-      event.target.innerHTML ===
-      '<path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>'
-    ) {
-      setPage(page - 1);
-    } else if (
-      event.target.innerHTML ===
-      '<path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>'
-    ) {
-      setPage(page + 1);
-    } else {
-      setPage(parseInt(event.target.outerText));
-    }
-  };
-
   return (
     <div className="QA">
       <div className="questions-body">
         <div className="qContents">
           <Accordion defaultActiveKey={["0"]} alwaysOpen>
-            <Question resource={data.data} />
+            <Question resource={data.data.question_list} />
           </Accordion>
         </div>
       </div>
       <div className="pageController">
         <Pagination
-          count={maxPage}
+          count={Math.ceil(data.data.total / data.data.size)}
           variant="outlined"
           shape="rounded"
           defaultPage={1}
-          onChange={(e) => handlePage(e)}
+          page={page}
+          onChange={(e, value) => setPage(value)}
         />
       </div>
     </div>
@@ -75,19 +61,18 @@ const Question = ({ resource }) => {
     <>
       {info.map((e) => {
         return (
-          <Accordion.Item eventKey={e.question.id}>
+          <Accordion.Item eventKey={e.id}>
             <div className="Head-Ac">
               <Accordion.Header>
                 <span style={{ paddingRight: "7px", fontWeight: "600" }}>
                   Q :
                 </span>
-                <div
-                  style={{ fontWeight: "600" }}
-                  dangerouslySetInnerHTML={{
-                    __html: e.question.title,
-                  }}
-                ></div>
+                <div style={{ fontWeight: "600" }}>{e.title}</div>
               </Accordion.Header>
+              {/* {Todo : 질문 채택 및 표시} */}
+              <div className="check">
+                <BsDashCircle size={25} color="grey" />
+              </div>
               {/* <div className="check">
             {info.check ? (
               <BsCheckCircle size={25} color="skyblue" />
@@ -98,17 +83,24 @@ const Question = ({ resource }) => {
             </div>
 
             <Accordion.Body>
+              {"질문"}
               <div
                 className="q_content"
                 dangerouslySetInnerHTML={{
-                  __html: e.question.question,
+                  __html: e.question,
                 }}
               ></div>
+              {"코드"}
+              <div className="q_content">
+                <CodeMirror value={e.code} editable={false} />
+              </div>
+
               {/* info에 들어있는 answer 배열로 넘겨줘서 map으로 answer 띄워주면 될듯*/}
+              {"답변"}
               {e.answers.map((ans) => {
                 return <Answer info={ans} />;
               })}
-              <MakeAnswer room_id={path.at(-1)} q_id={e.question.id} />
+              <MakeAnswer room_id={path.at(-1)} q_id={e.id} />
             </Accordion.Body>
           </Accordion.Item>
         );
@@ -118,7 +110,6 @@ const Question = ({ resource }) => {
 };
 
 const Answer = ({ info }) => {
-  console.log(info);
   return (
     <div className="ans">
       <div

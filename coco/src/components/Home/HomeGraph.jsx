ef11123019df533c3a3288ae4d4ba57fed1d4674
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-10-22 23:36:10 +0900

    <feat> : 그래프 설명
    1. main 페이지 그래프 설명 추가

--- coco/src/components/Home/HomeGraph.jsx
+++ coco/src/components/Home/HomeGraph.jsx
@@ -18,29 +18,34 @@ export const HomeGraph = ({ growth }) => {
   const getGrowth = () => {
     //맨 마지막 월 찾고 거기서 부터 계속 -1해가면서 저장
     let lastMonth = growth[0][0];
-    if(lastMonth === "0000"){
+    if (lastMonth === "0000") {
       let tmp = new Date();
-      lastMonth = tmp.getMonth()+1;
-    }else{
+      lastMonth = tmp.getMonth() + 1;
+    } else {
       lastMonth = lastMonth[2] + lastMonth[3];
     }
     var lineResult = [];
 
-    for(let i=0;i<growth.length;i++){
+    for (let i = 0; i < growth.length; i++) {
       lineResult.push({
-        name: month.at(lastMonth-1-i),
+        name: month.at(lastMonth - 1 - i),
         "COCO point": growth[i][1],
-      })
+      });
     }
 
     lineResult.reverse();
-    
+
     return lineResult;
   };
 
   return (
     <div className="lineG" style={{ width: "100%" }}>
-      <p>내 성장 그래프</p>
+      <p>
+        내 성장 그래프{" "}
+        <span style={{ color: "gray", fontSize: "0.9em" }}>
+          (난이도 x 문제 수)
+        </span>
+      </p>
       <ResponsiveContainer width="90%" height="100%">
         <LineChart
           data={getGrowth()}

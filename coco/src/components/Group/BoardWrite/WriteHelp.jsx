b7bfd874cbce1907d46981b4fb90005c1f92e5dd
kkm0406 <sdjmc30412@naver.com>
2023-10-31 16:48:45 +0900

    <Feat> : 스터디룸 글쓰기 ---------------------------------------- 1. 이미지 업로드 기능 삭제
    ----------------------------------------
    1. 앞으로 할 일

--- coco/src/components/Group/BoardWrite/WriteHelp.jsx
+++ coco/src/components/Group/BoardWrite/WriteHelp.jsx
@@ -11,10 +11,6 @@ import Swal from "sweetalert2";
 import axios from "axios";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-// import Quill from "quill";
-// import ImageResize from "@looop/quill-image-resize-module-react";
-
-// Quill.register("modules/imageResize", ImageResize);
 
 export const WriteHelp = ({ title, room_id }) => {
   const [code, setCode] = useState(""); //작성한 코드
@@ -23,56 +19,6 @@ export const WriteHelp = ({ title, room_id }) => {
   const [quillValue, setquillValue] = useState(""); // 메인 설명 html State !필수
   const quillRef = useRef(); // quill editor에 접근하기 위한 ref
 
-
-  // const imageHandler = () => {
-  //   // 1. 이미지를 저장할 input type=file DOM을 만든다.
-  //   const input = document.createElement("input");
-
-  //   // 속성 써주기
-  //   input.setAttribute("type", "file");
-  //   input.setAttribute("accept", "image/*");
-  //   input.click(); // 에디터 이미지버튼을 클릭하면 이 input이 클릭된다.
-  //   // input이 클릭되면 파일 선택창이 나타난다.
-
-  //   // input에 변화가 생긴다면 = 이미지를 선택
-  //   input.addEventListener("change", async () => {
-  //     const file = input.files[0];
-  //     const editor = quillRef.current.getEditor(); // 에디터 객체 가져오기
-  //     //현재 에디터 커서 위치값을 가져온다
-  //     const range = editor.getSelection();
-  //     axios
-  //       .post(
-  //         API.IMAGEUPLOAD,
-  //         {
-  //           file: file, // 파일
-  //         },
-  //         {
-  //           headers: {
-  //             "Content-Type": `multipart/form-data; `,
-  //             Authorization: "Bearer " + userInfo.access_token,
-  //           },
-  //           params: {
-  //             type: 2,
-  //           },
-  //         }
-  //       )
-  //       .then((res) => {
-  //         const IMG_URL = res.data;
-  //         // 가져온 위치에 이미지를 삽입한다
-  //         editor.insertEmbed(range.index, "image", IMG_URL);
-  //         //커서를 한칸 뒤로 이동
-  //         editor.setSelection(range.index + 1);
-  //       })
-  //       .catch(() => {
-  //         //오류시에 안내 메시지를 삽입
-  //         editor.insertText(range.index, "이미지 업로드에 실패했습니다", {
-  //           color: "#ff0000",
-  //           bold: true,
-  //         });
-  //       });
-  //   });
-  // };
-  //userMemo를 써야 오류가 안난다.
   const quill_module = useMemo(() => {
     return {
       toolbar: {
@@ -91,14 +37,7 @@ export const WriteHelp = ({ title, room_id }) => {
 
           ["clean"],
         ],
-        // handlers: {
-        //   // 이미지 처리는 imageHandler로 처리
-        //   // image: imageHandler,
-        // },
       },
-      // imageResize: {
-      //   modules: ["Resize", "DisplaySize"],
-      // },
     };
   }, []);
 

8ec241f22aa718f7f888ba6dd34e7c38200caa59
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-10-31 18:55:19 +0900

    <fix> : otherLogic

--- coco/src/components/Result/OtherLogic.jsx
+++ coco/src/components/Result/OtherLogic.jsx
@@ -21,23 +21,19 @@ export const OtherLogic = ({ changeLogic, task_id, sub_id }) => {
           <BsX size={28} color="red" />
         </div>
       </div>
-      <div className="itemCode" style={{ position: "relative" }}>
-        <div className="un" style={{ marginLeft: "1vw" }}>
-          <TbListSearch color="blue" size={23} />
-          <h2>다른 로직의 코드</h2>
-        </div>
-        <div style={{ minHeight: "200px" }}>
-          <OCcontent task_id={task_id} sub_id={sub_id} />
-        </div>
+
+      <div className="un" style={{ marginLeft: "0.5vw" }}>
+        <TbListSearch color="blue" size={23} />
+        <h2>다른 로직의 코드</h2>
       </div>
-      <div className="itemCode">
-        <div className="un" style={{ marginLeft: "1vw" }}>
-          <TbListSearch color="blue" size={23} />
-          <h2>유사 로직의 코드</h2>
-        </div>
-        <div style={{ minHeight: "200px" }}>
-          <SCcontent task_id={task_id} sub_id={sub_id} />
-        </div>
+
+      <div className="un" style={{ marginLeft: "0.5vw" }}>
+        <TbListSearch color="blue" size={23} />
+        <h2>유사 로직의 코드</h2>
+      </div>
+
+      <div className="codeTwo">
+        <OCcontent task_id={task_id} sub_id={sub_id} />
       </div>
     </div>
   );
@@ -98,6 +94,7 @@ const OCcontent = ({ task_id, sub_id }) => {
       </div>
     );
   }
+
   function makeNoLine(arr) {
     if (arr.length == 0) {
       return "";
@@ -105,9 +102,9 @@ const OCcontent = ({ task_id, sub_id }) => {
       arr.sort((a, b) => {
         return b.distance - a.distance;
       });
-      console.log(arr);
+
       var max_dis = arr[0];
-      var dataArray = max_dis.code.split("\n");
+      var dataArray = max_dis.split("\n");
 
       var numberedData = dataArray
         .map((item, index) => {
@@ -135,105 +132,10 @@ const OCcontent = ({ task_id, sub_id }) => {
   ) : (
     <div className="OCcontent">
       <div className="OC-Code">
-        <pre className="R-Code">{makeNoLine(data.data)}</pre>
+        <pre className="R-Code">{makeNoLine("print()")}</pre>
       </div>
-    </div>
-  );
-};
-
-const SCcontent = ({ task_id, sub_id }) => {
-  const [isOn, setIsOn] = useState(false);
-
-  const { isFetching, data } = useQuery(["OC", sub_id], () =>
-    axios
-      .post(
-        API.BASE_URL + "/code-cluster/main",
-        {},
-        {
-          params: {
-            task_id: task_id,
-            sub_id: sub_id,
-          },
-        }
-      )
-      .then(() => {
-        setIsOn(true);
-      })
-      .catch(() => {
-        Swal.fire({
-          icon: "error",
-          title: "AI Plugin 사용 불가\n\n404 NOT FOUND",
-        }).then((res) => {
-          if (res.isConfirmed) {
-            return <Notfound />;
-          }
-        });
-      })
-  );
-  if (isFetching) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-          minHeight: "120px",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <p
-          style={{
-            fontFamily: "Pretendard-Regular",
-            margin: "0",
-            fontSize: "1.2em",
-            fontWeight: "600",
-          }}
-        >
-          is Loading...
-          <br />
-          AI가 코드를 찾고 있습니다.
-        </p>
-      </div>
-    );
-  }
-  function makeNoLine(arr) {
-    if (arr.length == 0) {
-      return "";
-    } else {
-      arr.sort((a, b) => {
-        return b.distance - a.distance;
-      });
-      console.log(arr);
-      var max_dis = arr[0];
-      var dataArray = max_dis.code.split("\n");
-
-      var numberedData = dataArray
-        .map((item, index) => {
-          return `${index + 1}@${item}`;
-        })
-        .join("\n");
-
-      const strings = numberedData.split("\n").map((str) => {
-        const [num, val] = str.split("@");
-        return (
-          <div className="codeLine">
-            <n className="codeNum">{num}.</n>
-            <n className="codeTxt">{val}</n>
-          </div>
-        );
-      });
-      return strings;
-    }
-  }
-
-  return isOn === false ? (
-    <div id="sorry" style={{ display: "none" }}>
-      <Sorry />
-    </div>
-  ) : (
-    <div className="OCcontent">
       <div className="OC-Code">
-        <pre className="R-Code">{makeNoLine(data.data)}</pre>
+        <pre className="R-Code">{makeNoLine("print()")}</pre>
       </div>
     </div>
   );
